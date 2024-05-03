c4623dc6d30615fd3da998ba11c441a2a23b5c45
Amine Roudani <108030460+amineroudani@users.noreply.github.com>
2024-05-03 13:25:22 +0200

    object class and triangleindices update...

--- main.cpp
+++ main.cpp
@@ -64,6 +64,40 @@ public:
     Vector direction;
 };
 
+class Object{
+    public:
+        Object(Vector rho, bool mirror) : rho(rho), mirror(mirror) {};
+        virtual bool intersect(const Ray &r, Intersection &I) const = 0;
+        Vector rho;
+        bool mirror;
+};
+
+class TriangleIndices {
+public:
+	TriangleIndices(int vtxi = -1, int vtxj = -1, int vtxk = -1, int ni = -1, int nj = -1, int nk = -1, int uvi = -1, int uvj = -1, int uvk = -1, int group = -1, bool added = false) : vtxi(vtxi), vtxj(vtxj), vtxk(vtxk), uvi(uvi), uvj(uvj), uvk(uvk), ni(ni), nj(nj), nk(nk), group(group) {
+	};
+	int vtxi, vtxj, vtxk; // indices within the vertex coordinates array
+	int uvi, uvj, uvk;  // indices within the uv coordinates array
+	int ni, nj, nk;  // indices within the normals array
+	int group;       // face group
+};
+
+class BVHNode {
+public:
+    BoundingBox bounding_box;
+    BVHNode* left;
+    BVHNode* right;
+    size_t start;
+    size_t end;
+
+    BVHNode() : left(nullptr), right(nullptr), start(-1), end(-1) {}
+
+    ~BVHNode() {
+        delete left;
+        delete right;
+    }
+};
+
 class Intersection {
 public:
     Intersection(Vector P = Vector(0, 0, 0), Vector N = Vector(0, 0, 0), double t = std::numeric_limits<double>::max(), Vector rho = Vector(0, 0, 0))
